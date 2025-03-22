fa61d04f1c9ac6ad775c4c4a539101335d4960da
Dhiraj Kumar <darkdeveloperdj@gmail.com>
2025-03-22 22:38:02 +0530

    Add redux-persist for state persistence in the Redux store

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,5 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
+import { persistStore, persistReducer } from "redux-persist";
+
+// Conditionally require storage only on the client-side
+let storage;
+if (typeof window !== "undefined") {
+  storage = require("redux-persist/lib/storage").default;
+} else {
+  // Create a noop storage for server-side rendering
+  storage = {
+    getItem: () => Promise.resolve(null),
+    setItem: () => Promise.resolve(),
+    removeItem: () => Promise.resolve(),
+  };
+}
 
 import ContactSlice from "../store/contact/contactSlice";
 import contactSaga from "../store/contact/contactSaga";
@@ -7,20 +21,41 @@ import contactSaga from "../store/contact/contactSaga";
 import productsSlice from "../store/product/productsSlice";
 import productsSaga from "../store/product/productSaga";
 
+// Create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
 
-// Configure store
+// Define your persist configuration
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["contact", "products"], // Only these slices will be persisted
+};
+
+// Combine your reducers
+const rootReducer = combineReducers({
+  contact: ContactSlice,
+  products: productsSlice,
+});
+
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure store with the persisted reducer and saga middleware
 const store = configureStore({
-  reducer: {
-    contact: ContactSlice,
-    products: productsSlice,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware), // Disable thunk, add saga middleware
+    getDefaultMiddleware({
+      thunk: false,
+      // Disable serializable check for redux-persist actions
+      serializableCheck: false,
+    }).concat(sagaMiddleware),
 });
 
 // Run the Saga middleware
 sagaMiddleware.run(contactSaga);
-sagaMiddleware.run(productsSaga); // Add this line to run the product saga
+sagaMiddleware.run(productsSaga);
+
+// Create the persistor for the store
+export const persistor = persistStore(store);
 
 export default store;
